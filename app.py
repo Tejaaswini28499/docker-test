ea44f099bc094f815f582307d611d02514a3cbb2
Tejaaswini28499 <52596535+Tejaaswini28499@users.noreply.github.com>
2025-09-19 13:32:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("hello tejaaswini")
+import redis
+
+# Connect to Redis (host = service name in docker-compose.yml)
+r = redis.Redis(host='redis', port=6379)
+
+# Increment a counter
+count = r.incr('hits')
+
+print(f"Hello, World! This app has been run {count} times.")
