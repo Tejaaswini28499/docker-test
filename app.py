3f39e3e1c1b47b99774f61e62125469884c8143b
Tejaaswini28499 <52596535+Tejaaswini28499@users.noreply.github.com>
2025-09-19 15:49:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import time
 import redis
 
-# Connect to Redis (host = service name in docker-compose.yml)
-r = redis.Redis(host='redis', port=6379)
+r = None
+for i in range(5):  # retry 5 times
+    try:
+        r = redis.Redis(host='redis', port=6379)
+        r.ping()  # check if Redis is ready
+        break
+    except redis.exceptions.ConnectionError:
+        print("Redis not ready, retrying...")
+        time.sleep(2)
 
-# Increment a counter
+if not r:
+    raise Exception("Cannot connect to Redis")
+
 count = r.incr('hits')
-
 print(f"Hello, World! This app has been run {count} times.")
